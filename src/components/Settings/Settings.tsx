b867aa8cd0dd63d2d9a8b302be3048935a640159
Te1epuzik <tonkiyslonik@gmail.com>
2024-07-24 01:44:17 +0300

    deploy fix

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react";
 import classes from "./settings.module.scss";
 import { SettingsView } from "./SettingsView";
-import { useGetAllData, useSetHall } from "@/services";
-import { THall } from "@/models/SessionsModel";
+import { useGetAllData } from "@/services";
 import { Loader, E404 } from "@/components";
 
 export const Settings = () => {
 	const allData = useGetAllData();
 	
-
   return (
     <>
       {allData.isLoading && (
